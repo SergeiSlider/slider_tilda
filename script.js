2e146c244627b75f2b043e4d23d389bc5053ac33
SergeiSlider <88104979+SergeiSlider@users.noreply.github.com>
2021-07-28 19:51:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,162 @@
+$(function () {
+    var delete_tag = '<img class="delete_slide" src="https://evgeniyvorobev.github.io/tilda_slider_add_remove/img/delete_2.png">';
+
+    window.$selected_slider;
+    window.$element_clicked;
+    window.$lastSlide;
+    window.slider_current = 0;
+    window.$current_slider = '';
+
+    $('.slick_sliders').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        nextArrow: `<div class="right">
+            <svg style="display: block" viewBox="0 0 9.3 17" xmlns="http://www.w3.org/2000/svg"
+                 xmlns:xlink="http://www.w3.org/1999/xlink">
+                <desc>Right</desc>
+                <polyline fill="none" stroke="#000000" stroke-linejoin="butt" stroke-linecap="butt" stroke-width="1"
+                          points="0.5,0.5 8.5,8.5 0.5,16.5"></polyline>
+            </svg>
+        </div>`,
+        prevArrow: `<div class="left">
+            <svg style="display: block" viewBox="0 0 9.3 17" xmlns="http://www.w3.org/2000/svg"
+                 xmlns:xlink="http://www.w3.org/1999/xlink">
+                <desc>Left</desc>
+                <polyline fill="none" stroke="#000000" stroke-linejoin="butt" stroke-linecap="butt" stroke-width="1"
+                          points="0.5,0.5 8.5,8.5 0.5,16.5"></polyline>
+            </svg>
+        </div>`
+    })
+
+    $('.slick_sliders').on('swipe', function(event, slick, currentSlide, nextSlide){
+        console.log('это слайдер slick', slick.$slider)
+        $current_slider = slick.$slider;
+        $current_slider.find('.delete_slide').hide();
+        if ($current_slider.find('img.img_original.slick-slide.slick-current.slick-active').is('.hovered')) {
+            setTimeout(function () {
+                $current_slider.find('.delete_slide').show();
+            },500)
+        } else {
+            $current_slider.find('img.img_original.slick-slide.slick-current.slick-active').removeClass('hovered');
+            $current_slider.find('.delete_slide').hide();
+        }
+    });
+
+    $('.slick_sliders').on('swipe', function(event, slick, currentSlide, nextSlide){
+        console.log('это слайдер slick', slick.$slider)
+        $current_slider.find('.delete_slide').hide();
+        if ($current_slider.find('img.img_original.slick-slide.slick-current.slick-active').is('.hovered')) {
+            setTimeout(function () {
+                $current_slider.find('.delete_slide').show();
+            },500)
+        } else {
+            $current_slider.find('img.img_original.slick-slide.slick-current.slick-active').removeClass('hovered');
+            $current_slider.find('.delete_slide').hide();
+        }
+    });
+
+    $('.slick_sliders').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        console.log('is hovered?' , slick.$slider.find('[data-slick-index='+nextSlide+']').is('.hovered'))
+        if (slick.$slider.find('[data-slick-index='+nextSlide+']').is('.hovered')) {
+            setTimeout(function () {
+                $current_slider.find('.delete_slide').show();
+            },500)
+        } else {
+            slick.$slider.find('.delete_slide').hide();
+        }
+    });
+
+    $('.add_img').on('click', function (e) {
+        $element_clicked = $(e.target);
+        $('#slider_upload').click();
+    });
+
+    /* Load file to slider */
+    window.loadFile = function (e) {
+        // слайдер с которым сейчас работаем
+        $selected_slider = $element_clicked.parents('.slick-slider');
+        // url загруженной фото в хранилище браузера
+        console.log('e.target.files[0] ', e.target.files[0]);
+        if (e.target.files[0] != undefined) {
+            var file_url = URL.createObjectURL(e.target.files[0]);
+            // добавление слайда в слайдер.
+            $selected_slider.slick('slickAdd', '<img class="img_original" src="' + file_url + '">', '0');
+            // номер последнего добавленно слайда
+            lastSlide = $selected_slider.find('.slick-slide').not('.slick-cloned').last().data().slickIndex;
+            console.log(lastSlide);
+            // перемотка к добавленному слайду.
+            $selected_slider.slick('slickGoTo', lastSlide)
+            addHover();
+        }
+    }
+
+    /* Hover on image functions */
+    function addHover() {
+        $('.slick-track img.img_original').off();
+        $('.slick-track img.img_original').hover(hoverIn, hoverOut);
+    }
+
+    function hoverIn(e) {
+        let $target = $(e.target);
+        $current_slider = $target.parents('.slick-slider');
+        console.log($current_slider)
+        $current_slider.find('img.img_original.slick-slide.slick-current.slick-active').addClass('hovered');
+        // добавляем тег удаления слайда
+        if (!$current_slider.find('.delete_slide').is('.delete_slide')) {
+            $current_slider.prepend(delete_tag);
+            $current_slider.find('img.delete_slide').on('click', function (e) {
+                $current_slider = $(e.target).parents('.slick-slider');
+              if ( confirm('Вы хотите удалить изображение?') ) {
+                  deleteSlide();
+              } else {
+                  $current_slider.find('img.img_original.slick-slide.slick-current.slick-active').removeClass('hovered');
+                  $current_slider.find('.delete_slide').hide();
+              }
+            })
+        }
+        $current_slider.find('.delete_slide').show();
+    }
+
+    function hoverOut(e) {
+        let $target = $(e.target);
+        let $targetOut = $(document.elementFromPoint(e.clientX, e.clientY));
+
+        // for mobile
+        if ($(window).width() < 640 ) {
+            $current_slider = $targetOut.parents('.slick-slider');
+        }
+
+        console.log('$targetOut.is(\'body\')', $targetOut.is('body'));
+        console.log('$targetOut)', $targetOut);
+
+        if ($targetOut.is('.right.slick-arrow') || $targetOut.is('svg') || $targetOut.is('.left.slick-arrow')) {
+            $current_slider.find('.delete_slide').hide();
+            return false;
+        }
+
+        console.log('$targetOut', $(document.elementFromPoint(e.pageX, e.pageY)))
+        if ($targetOut.is('.delete_slide')) {
+            return false;
+        } else {
+            console.log('Работаем с этим слайдером $current_slider',$current_slider);
+            $current_slider.find('img.img_original.slick-slide.slick-current.slick-active').removeClass('hovered');
+            $current_slider.find('.delete_slide').hide();
+        }
+    }
+
+    function deleteSlide() {
+        $current_slider.find('.slick-slide').not('.slick-cloned').not('.add_img ').each(function(i,d){
+            slider_current++;
+            console.log(i,d, slider_current);
+            if ($(d).is('.slick-active') && $(d).is('.hovered')) {
+                $current_slider.slick('slickRemove', slider_current);
+                slider_current = 0;
+                return false;
+            }
+        })
+        $current_slider.find('.delete_slide').hide();
+        $current_slider.find('img.img_original.slick-slide.slick-current.slick-active').removeClass('hovered');
+    }
+
+})
